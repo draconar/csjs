ebc6869ee24cb684162b3835f4197e0577f09b6e
Fabio "Draco" Fonseca <draconar@outlook.com>
2014-11-22 13:33:24 -0200

    bugs
    many bugs solved in both BFS and DFS

--- graph.js
+++ graph.js
@@ -41,9 +41,35 @@ Graph.prototype.dfs = function(v) {
 		console.log(v + " is visited");
 		for(var w in this.adj[v]){
 			//if it was not visited
-			if (!this.marked[w]){
-				this.dfs(w);
+			if (!this.marked[this.adj[v][w]]){
+				this.dfs(this.adj[v][w]);
 			}			
 		}
 	}
 }
+
+Graph.prototype.bfs = function(s) {
+	var queue = [];
+	this.marked[s] = true;
+	queue.push(s);
+	while(queue.length > 0){
+		var v = queue.shift();
+		if (v != undefined) {
+			console.log ("visited vertex: " + v);
+		}
+		for( var w = 1; w< this.adj[v].length; w++){
+			if(!this.marked[this.adj[v][w]]){
+				this.marked[this.adj[v][w]] = true;
+				queue.push(this.adj[v][w]);
+				
+			}
+		}
+	}
+}
+
+g = new Graph(5);
+g.addEdge(0,1);
+g.addEdge(0,2);
+g.addEdge(1,3);
+g.addEdge(2,4);
+
